f14e60c9dee31996244ab574b8df3d9e46be1eb0
RahulKumarMishra <kumarrahul.allduniv@gmail.com>
2025-02-01 19:00:01 +0530

    added path functionality

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
+    let mut directories:Vec<&str> = Vec::new();
+    let builin_commands = vec!["echo", "type", "exit"];
     loop {
     print!("$ ");
     io::stdout().flush().unwrap();
@@ -14,6 +16,10 @@ fn main() {
     if input.trim() == "exit 0" {
         break;
     }
+    else if input.starts_with("PATH=") {
+        directories = input.trim().split("=").collect::<Vec<&str>>()[1].split(":").collect::<Vec<&str>>();
+
+    }
     else if input.starts_with("echo") {
         let content  = input.trim().split(" ").collect::<Vec<&str>>();
         println!("{}", content[1..content.len()].join(" "));
@@ -21,11 +27,22 @@ fn main() {
     else if input.starts_with("type") {
         let content  = input.trim().split(" ").collect::<Vec<&str>>();
         //println!("command - type {}", content[1]);
-        match content[1] {
-            "echo" => println!("echo is a shell builtin"),
-            "type" => println!("type is a shell builtin"),
-            "exit" => println!("exit is a shell builtin"),
-            _ => println!("{}: not found", content[1]),
+        if builin_commands.contains(&content[1]) {
+            println!("{} is a shell builtin", content[1]);
+        }
+        else {
+            let mut found = false;
+            for dir in &directories {
+                let path = format!("{}/{}", dir, content[1]);
+                if std::path::Path::new(&path).exists() {
+                    println!("{} is {}", content[1], path);
+                    found = true;
+                    break;
+                }
+            }
+            if !found {
+                println!("{}: command not found", content[1]);
+            }
         }
     }
     else {

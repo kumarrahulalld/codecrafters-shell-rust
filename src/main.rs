e7868ffbbe48b03a00244f89e8ccd1c62fa8c80d
RahulKumarMishra <kumarrahul.allduniv@gmail.com>
2025-02-01 19:05:02 +0530

    added path functionality

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    println!("input {}", input.trim());
     if input.trim() == "exit 0" {
         break;
     }

136693cbdb42a9d4f13f8aed0f580fd4ef0953f5
RahulKumarMishra <kumarrahul.allduniv@gmail.com>
2025-02-24 23:20:46 +0530

    added redirect support

--- src/main.rs
+++ src/main.rs
@@ -130,7 +130,7 @@ fn handle_pwd(redirect_file: Option<String>) {
             let mut file = File::create(file).unwrap();
             writeln!(file, "{}", output).unwrap();
         } else {
-            print!("{}", output); 
+            println!("{}", output); 
         }
     } else {
         eprintln!("pwd: error retrieving current directory");
